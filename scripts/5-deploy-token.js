5126d1e8607609d36a82b54e8d93b4a2e36c8624
Thaís Rodeiro <ttheinterpreter@gmail.com>
2022-05-06 16:18:11 -0300

    Deploying Token

---
+++ scripts/5-deploy-token.js
@@ -0,0 +1,17 @@
+import { AddressZero } from "@ethersproject/constants";
+import sdk from "./1-initialize-sdk.js";
+
+(async () => {
+    try {
+        const tokenAddress = await sdk.deployer.deployToken({
+            name: "WalkUrCatDAO Governance Token",
+            symbol: "WHI$KERS",
+            primary_sale_recipient: AddressZero,
+        });
+        console.log(
+            "✅ Successfully deployed token module, address:", tokenAddress
+        );
+    } catch (error) {
+        console.error("failed to deploy token module", error);
+    }
+})();
